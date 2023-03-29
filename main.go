a21aabe488a9571bdb578c4019db68fc0e64ca28
TAB <tab@tab.com>
2023-03-28 22:26:27 -0400

    Add in placeholder REST methods

--- main.go
+++ main.go
@@ -1,9 +1,48 @@
 package main
 
-import {
+import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
-}
+)
 
 func main() {
-	
+
+	r := gin.Default()
+
+	v1 := r.Group("/v1")
+	{
+
+		v1.GET("bourbon", getBourbon)
+		v1.GET("bourbon/:id", getBourbonById)
+
+		v1.POST("bourbon", addBourbon)
+		v1.PUT("bourbon/:id", updateBourbon)
+		v1.DELETE("bourbon/:id", deleteBourbon)
+
+	}
+
+	r.Run()
+}
+
+func getBourbon(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "getBourbons Called"})
+}
+
+func getBourbonById(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "getBourbonById " + id + " Called"})
+}
+
+func addBourbon(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "addBourbon Called"})
+}
+
+func updateBourbon(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message": "updateBourbon Called"})
+}
+
+func deleteBourbon(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(http.StatusOK, gin.H{"message": "deleteBourbon " + id + " Called"})
 }
